d52e105a6e5b14b6ab6ac5012199e79e3ddbc697
Badar Hossain <badar12041@gmail.com>
2024-02-25 19:39:13 +0600

    Practice task 2 completed

--- spread.js
+++ spread.js
@@ -15,11 +15,40 @@ console.log(newNumbers);
 
 const person = {
     name: 'Badar',
+    status: 'Developer'
+
 };
 
 const newPerson = {
     ...person,
-    age: 28
+    age: 28,
+    address: {
+        city: 'Karachi',
+        country: 'Pakistan'
+    
+    }
 };
 
 console.log(newPerson);
+
+const {age: newAge} = newPerson;
+console.log(newAge); //Output: 28
+
+const{address:{city: newCity}} = newPerson; //Output: Karachi //Nested object destructuring 
+
+
+
+
+const arr=[1, 2, 3, 4, 5];
+
+const [a, b, ...rest] = arr;
+console.log(a,b,rest);
+
+//Output: 1 2 [3, 4, 5]
+
+//rest operator is used to merge a list of function arguments into an array.
+
+
+
+
+
