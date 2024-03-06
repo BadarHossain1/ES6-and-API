e6f0d50de0685bb2e98e58434b57a88ab06345ff
Badar Hossain <badar12041@gmail.com>
2024-03-06 21:30:56 +0600

    All in one revision

--- AllInOneES6.js
+++ AllInOneES6.js
@@ -7,7 +7,7 @@ const student = {
 
 // 1: Template String
 
-const about = `My Name is ${student.name}. I am ${student.age} years old. I am a ${student.position} Instructor. I have ${numbers.length} students in my class.`
+const about = `My Name is ${student.name}. I am ${student.age} years old. I am a ${student.position} Instructor.`
 
 // 2: Arrow Function
 
@@ -48,3 +48,47 @@ const cheap = products.filter(product => product.price <= 5000);
 // 4: Find
 
 const special = products.find(product => product.name === 'laptop');
+
+
+// Array destructuring
+
+const numbers1 = [42,65];
+
+// const x = numbers1[0];
+// const y = numbers1[1];
+
+const [x, y] = numbers1;
+
+function boxify(num1, num2){
+    const nums = [num1, num2];
+    return nums;
+}
+
+const [first, second] = boxify(90, 34);
+
+// Object Destructuring
+
+const {name, price} = {name: 'laptop', price: 3200, brand: 'lenovo', color: 'silver'};
+console.log(name, price);
+
+const employee = {  
+    ide: 'VS Code',
+    designation: 'developer',
+    machine: 'mac',
+    language: ['html', 'css', 'js'],
+    specification: {
+        height: 66,
+        weight: 60,
+        address: 'kumarkhali',
+        drink: 'water',
+        watch: {
+            color: 'black',
+            price: 500,
+            brand: 'garmin'
+        }
+    }
+}
+
+const {machine, ide} = employee;
+const {weight, address} = employee.specification;
+const {brand} = employee?.specification?.watch; //optional chaining operator (?.)
